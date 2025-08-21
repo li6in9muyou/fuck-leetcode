d07f995dd0f068c4b1213f7569a118430cf9ee7e
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-21 20:43:02 +0800

    wip: still bugged

--- 1000.js
+++ 1000.js
@@ -7,46 +7,51 @@ function scheduler(max) {
   const loop = async function () {
     --DEBUG_LIMIT > 0 && setTimeout(loop, 0);
 
-    while (workQ.length < max) {
-      workQ.push(taskQ.unshift());
+    if (workQ.length === max) {
+      return;
+    }
+
+    while (workQ.length < max && taskQ.length > 0) {
+      workQ.push(taskQ.shift());
     }
 
     console.log("libq scheduler/racebegin", workQ, taskQ);
-    const [ans, onComplete, getTask] = await Promise.race(
-      workQ.map((fn) => fn()),
+    const done = await Promise.race(
+      workQ.map(([fn, onComplete]) =>
+        fn().then((ans) => [ans, onComplete, fn]),
+      ),
     );
-    console.log("libq scheduler/racedone", ans, onComplete, promise);
+    console.log("libq scheduler/racedone", done);
+
+    const [ans, onComplete, getTask] = done;
 
     onComplete(ans);
 
-    const remaining = taskQ.filter((q) => q.getTask !== getTask);
+    const remaining = taskQ.filter((q) => q !== getTask);
     workQ = remaining.concat(workQ);
   };
 
   loop();
 
   return function (getTask) {
-    taskQ.push(getTask);
-
     let _r;
     const p = new Promise((r) => (_r = r));
-    p.then((ans) => [ans, _r, getTask]);
-
+    taskQ.push([getTask, _r]);
     return p;
   };
 }
 
 const run = scheduler(2);
 run(() => sleep(3000, 1));
-run(() => sleep(2000, 2));
-run(() => sleep(2000, 3));
-run(() => sleep(2000, 4));
-run(() => sleep(2000, 5));
-run(() => sleep(2000, 6));
-run(() => sleep(2000, 7));
-run(() => sleep(2000, 8));
-run(() => sleep(2000, 9));
-run(() => sleep(2000, 10));
+run(() => sleep(500, 2));
+run(() => sleep(500, 3));
+run(() => sleep(500, 4));
+run(() => sleep(500, 5));
+run(() => sleep(500, 6));
+run(() => sleep(500, 7));
+run(() => sleep(500, 8));
+run(() => sleep(500, 9));
+run(() => sleep(500, 10));
 
 function sleep(ms, ans) {
   return new Promise((resolve) => {
