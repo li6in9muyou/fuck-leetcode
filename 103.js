d3cfcf8cccf10dff0fbafe0dc4cab60c90d8a1f6
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-21 21:45:06 +0800

    ok: add

---
+++ 103.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (root === null) {
+    return [];
+  }
+
+  const ans = [];
+
+  let ltrOrRtl = true;
+  let q = [root];
+  while (q.length > 0) {
+    const thisLevel = [...q];
+    const thisLevelVal = thisLevel.map((n) => n.val);
+    q = [];
+
+    if (!ltrOrRtl) {
+      thisLevelVal.reverse();
+    }
+    ans.push(thisLevelVal);
+
+    ltrOrRtl = !ltrOrRtl;
+    for (const node of thisLevel) {
+      node.left && q.push(node.left);
+      node.right && q.push(node.right);
+    }
+  }
+
+  return ans;
+};
