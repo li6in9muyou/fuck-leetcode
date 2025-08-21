c03cd6b2f2a9136b2ff4b382c5400ebf5ab2f6c2
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-21 22:21:43 +0800

    wip: add

---
+++ 105.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  return descend(preorder, inorder);
+
+  function descend(pre, mid) {
+    if (mid.length === 0) {
+      return null;
+    }
+
+    const me = pre[0];
+    const meIdxInMid = mid.indexOf(me);
+
+    const left = descend(pre.slice(1), mid.slice(0, meIdxInMid));
+    const right = descend(pre.slice(meIdxInMid + 1), mid.slice(meIdxInMid + 1));
+
+    const meNode = new TreeNode(me);
+    meNode.left = left;
+    meNode.right = right;
+
+    return meNode;
+  }
+};
---
+++ 106.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function (inorder, postorder) {
+  return descend(inorder, postorder);
+
+  function descend(mid, post) {
+    if (mid.length === 0 || post.length === 0) {
+      return null;
+    }
+
+    const I = post.length - 1;
+    const me = post[I];
+    const meIdx = mid.indexOf(me);
+
+    const leftCnt = meIdx;
+    const rightCnt = I + 1 - meIdx;
+
+    const left = descend(mid.slice(0, leftCnt), post.slice(0, leftCnt));
+    const right = descend(
+      mid.slice(meIdx + 1),
+      post.slice(leftCnt + 1, leftCnt + rightCnt),
+    );
+
+    const meNode = new TreeNode(me);
+    meNode.left = left;
+    meNode.right = right;
+
+    return meNode;
+  }
+};
