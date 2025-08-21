0bc8dd58e434f5ecb22a906b8a41ec1e0fa0756a
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-21 21:29:38 +0800

    refactor: rename

--- 1000.js
+++ 1000.js
@@ -19,43 +19,49 @@ function scheduler(max) {
       }
 
       while (working.size < max && taskQueue.length > 0) {
-        const { task, resolve } = taskQueue.shift();
+        const { getPromise, onComplete } = taskQueue.shift();
 
-        const w = { resolve };
-        w.promise = task().then((ans) => {
+        const w = {};
+        w.promise = getPromise().then((ans) => {
           w.ans = ans;
-          return w;
+          return w.self;
         });
+        w.self = w;
+        w.onComplete = onComplete;
+
         working.add(w.promise);
       }
       if (working.size > 0) {
-        const done = await Promise.race(working);
-        working.delete(done.promise);
-        const { ans, resolve } = done;
-        resolve(ans);
+        const { ans, onComplete, promise } = await Promise.race(working);
+        working.delete(promise);
+        onComplete(ans);
       }
     }
   })();
 
-  return function (task) {
+  return function (getPromise) {
     return new Promise((resolve) => {
-      taskQueue.push({ task, resolve });
+      taskQueue.push({ getPromise, onComplete: resolve });
       console.log("libq scheduler/enqueue", taskQueue);
     });
   };
 }
 
 const run = scheduler(3);
-run(() => sleep(2000, 1)).then((x) => console.log("libq run/done", x));
-run(() => sleep(500, 2)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 3)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 4)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 5)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 6)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 7)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 8)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 9)).then((x) => console.log("libq run/done", x));;
-run(() => sleep(500, 10)).then((x) => console.log("libq run/done", x));;
+run(() => sleep(5000, 1)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 2)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 3)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 4)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 5)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 6)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 7)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 8)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 9)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 10)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 11)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 12)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 13)).then((x) => console.log("libq run/done", x));
+run(() => sleep(500, 14)).then((x) => console.log("libq run/done", x));
 
 function sleep(ms, ans) {
   return new Promise((resolve) => {
