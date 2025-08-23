74d7ddc399617421e44cc16fe558a0a862923da1
li6in9muyou <libq5520@mails.jlu.edu.cn>
2025-08-23 17:15:07 +0800

    ok

--- 53.js
+++ 53.js
@@ -7,41 +7,20 @@ const maxSubArray = (arr) => {
     return arr.reduce((a, b) => a + b, 0);
   }
   if (arr.every((x) => x <= 0)) {
-    return 0;
+    return Math.max(...arr);
   }
 
-  const maxIfEndHere = [0];
-  const currMaxLastNumIdx = [0];
-  for (let i = 0; i < arr.length; i++) {
-    maxIfEndHere.push(0);
-    currMaxLastNumIdx.push(0);
-  }
+  const maxIfEndHere = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    const extend = maxIfEndHere[i - 1] + arr[i];
+    const reset = arr[i];
 
-  for (let i = 0; i < arr.length; i++) {
-    const currNum = arr[i];
-    if (currNum >= 0) {
-      const extend =
-        currMaxLastNumIdx[i] === i - 1 ? maxIfEndHere[i] + currNum : -Infinity;
-      const reset = currNum;
-      let takeAll = maxIfEndHere[currMaxLastNumIdx[i]];
-      for (let j = currMaxLastNumIdx[i]; j <= i; j++) {
-        takeAll += arr[j];
-      }
-
-      currMaxLastNumIdx[i + 1] = i;
-      maxIfEndHere[i + 1] = Math.max(extend, reset, takeAll);
+    if (extend > reset) {
+      maxIfEndHere.push(extend);
     } else {
-      const keep = maxIfEndHere[i];
-      const reset = currNum;
-
-      if (keep > reset) {
-        currMaxLastNumIdx[i + 1] = currMaxLastNumIdx[i];
-      } else {
-        currMaxLastNumIdx[i + 1] = i;
-      }
-      maxIfEndHere[i + 1] = Math.max(keep, reset);
+      maxIfEndHere.push(reset);
     }
   }
 
-  return maxIfEndHere[arr.length];
+  return Math.max(...maxIfEndHere);
 };
